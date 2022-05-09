232856b38cbbd1d36ca0d543586bb4c1b520ac2d
Álvaro Ramos <alvaroramos2222@hotmail.com>
2022-05-09 12:54:20 -0300

    feat: requirement 6 complete

---
+++ desafio6.sql
@@ -0,0 +1,8 @@
+SELECT 
+    UCASE(ROUND(MIN(p.valor_plano), 2)) AS faturamento_minimo,
+    UCASE(ROUND(MAX(p.valor_plano),2)) AS faturamento_maximo,
+    UCASE(ROUND(AVG(p.valor_plano), 2)) AS faturamento_medio,
+    UCASE(ROUND(SUM(p.valor_plano), 2)) AS faturamento_total
+FROM SpotifyClone.plano AS p
+INNER JOIN SpotifyClone.usuario AS u
+ON p.plano_id = u.plano;
